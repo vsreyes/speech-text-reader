5221f4ab9366a3300b4640be91d79fa424463162
vsreyes <virgilio.s.reyes@gmail.com>
2020-08-23 20:29:16 +0200

    Added script

--- script.js
+++ script.js
@@ -33,5 +33,110 @@ const data = [
     {
         image: './img/sad.jpg',
         text: "I'm Sad"
+    },
+    {
+        image: './img/scared.jpg',
+        text: "I'm Scared"
+    },
+    {
+        image: './img/outside.jpg',
+        text: 'I Want To Go Outside'
+    },
+    {
+        image: './img/home.jpg',
+        text: 'I Want To Go Home'
+    },
+    {
+        image: './img/school.jpg',
+        text: 'I Want To Go To School'
+    },
+    {
+        image: './img/grandma.jpg',
+        text: 'I Want To Go To Grandmas'
     }
-]
+];
+
+data.forEach(createBox);
+
+// Create speech boxes
+function createBox(item) {
+    const box = document.createElement('div');
+
+    const { image, text } = item;
+
+    box.classList.add('box');
+
+    box.innerHTML = `
+        <img src="${image}" alt="${text}" />
+        <p class = "info">${text}</p>
+        `;
+
+    box.addEventListener('click', () => {
+        setTextMessage(text);
+        speakText();
+
+        // Add active effect
+        box.classList.add('active');
+        setTimeout(() => box.classList.remove('active'), 800);
+    });
+
+    main.appendChild(box);
+}
+
+    // Init speech synth
+    const message = new SpeechSynthesisUtterance();
+
+    // Init voices
+    let voices = [];
+
+    function getVoices() {
+        voices = speechSynthesis.getVoices();
+
+        voices.forEach(voice => {
+            const option = document.createElement('option');
+        
+        option.value = voice.name;
+        option.innerText = `${voice.name} ${voice.lang}`;
+
+        voiceSelect.appendChild(option);
+        });
+    }
+
+    // Set text
+    function setTextMessage(text) {
+        message.text = text;
+    }
+
+    // Speak text
+    function speakText() {
+        speechSynthesis.speak(message);
+    }
+
+    // Set voice
+    function setVoice(e) {
+        message.voice = voices.find(voice => voice.name === e.target.value);
+    }
+
+    // Voices changed
+    speechSynthesis.addEventListener('voiceschanged', getVoices);
+
+    // Toggle text box
+    toggleBtn.addEventListener('click', () =>
+        document.getElementById('text-box').classList.toggle('show')
+    );
+    
+    // Close button
+    closeBtn.addEventListener('click', () =>
+        document.getElementById('text-box').classList.remove('show')
+    );
+
+    // Change voice
+    voiceSelect.addEventListener('change', setVoice);
+
+    // Read text button
+    readBtn.addEventListener('click', () => {
+        setTextMessage(textarea.value);
+        speakText();
+    });
+
+    getVoices();
