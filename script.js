735d56f662285a1294ca31c185f58c2a4a2e677b
vsreyes <virgilio.s.reyes@gmail.com>
2020-08-23 16:31:36 +0200

    Added script

---
+++ script.js
@@ -0,0 +1,37 @@
+const main = document.querySelector('main');
+const voiceSelect = document.getElementById('voices');
+const textarea = document.getElementById('text');
+const readBtn = document.getElementById('read');
+const toggleBtn = document.getElementById('toggle');
+const closeBtn = document.getElementById('close');
+
+const data = [
+    {
+        image: './img/drink.jpg',
+        text: "I'm Thirsty"
+    },
+    {
+        image: './img/food.jpg',
+        text: "I'm Hungry"
+    },
+    {
+        image: './img/tired.jpg',
+        text: "I'm Tired"
+    },
+    {
+        image: './img/hurt.jpg',
+        text: "I'm Hurt"
+    },
+    {
+        image: './img/happy.jpg',
+        text: "I'm Happy"
+    },
+    {
+        image: './img/angry.jpg',
+        text: "I'm Angry"
+    },
+    {
+        image: './img/sad.jpg',
+        text: "I'm Sad"
+    }
+]
